907c0ee3f391aac210b13a08ce1f4ce4424bae8d
tod1az <t.diaz.soto@gmail.com>
2024-02-20 19:57:43 -0300

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/tod1az/go-api/internal/handlers"
+
+	log "github.com/sirupsen/logrus"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tod1az/go-api.git
+
+go 1.22.0
